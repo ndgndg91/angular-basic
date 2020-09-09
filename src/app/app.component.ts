8fe8ad1cedb57a3c8c7019cde6efd58e04210823
ndgndg91 <ndgndg91@gmail.com>
2020-09-09 20:43:22 +0900

    Angular Basic Project Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'This is My First Angular App';
+
+  firstName = "Dong-Gil";
+  lastName = "Nam";
 }
